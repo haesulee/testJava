5d5ad0bab0ad145d81aa950c6938dee1bc32088e
이해수 <haesulee@lgcns.com>
2021-02-25 09:53:23 +0900

    CICDTEST01-4 : https://cicdstudy.atlassian.net/browse/CICDTEST01-4

--- src/test/TestJava.java
+++ src/test/TestJava.java
@@ -4,14 +4,10 @@ public class TestJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, Java!");
-		System.out.println("Welcome to Git!");
-		System.out.println("test jira TEST01-1");
-		System.out.println("test jira TEST01-2");
-		System.out.println("comment jira issue ID"); 
-		System.out.println("Jenkins build and triggering CICDTEST01-3"); 
-		System.out.println("Jenkins build trigger local jira TE ST01-3"); 
-		
+		System.out.println("This is simple Java Sorce.");
+		System.out.println("compile with ANT scripts.");
+		System.out.println("send source to GITHUB.");
+		System.out.println("and inform to JIRA by addressing comments.");
 
 	}
 

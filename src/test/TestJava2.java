cc315029e92aa160345c3ecb8c9ade0185047fe4
haesulee <39815976+haesulee@users.noreply.github.com>
2021-03-17 16:34:21 +0900

    Update TestJava2.java
    이슈 ID : UT1-22-3003
    파일을 신규 작성하였습니다.

--- src/test/TestJava2.java
+++ src/test/TestJava2.java
@@ -10,6 +10,7 @@ public class TestJava2 {
 		System.out.println("test jira TEST01-2");
 		System.out.println("comment jira issue ID"); 
 		System.out.println("Jenkins build and triggering CICDTEST01-3"); 
+		// Jira 이슈 ID를 Comment에 남겨 봅니다.
 		System.out.println("이슈 ID를 포함하여 Comment를 남깁니다."); 	
 	}
 

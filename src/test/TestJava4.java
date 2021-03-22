e03fb8aef76a72c7173e295e03638c31d45bd521
haesulee <39815976+haesulee@users.noreply.github.com>
2021-03-22 10:53:09 +0900

    Create TestJava4.java
    이슈 ID : UT1-32
    CSR ID : CSR-210322
    완료하였습니다.

---
+++ src/test/TestJava4.java
@@ -0,0 +1,19 @@
+package test;
+
+public class TestJava4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, Java!");
+		System.out.println("Welcome to Git!");
+		System.out.println("test jira TEST01-1");
+		System.out.println("test jira TEST01-2");
+		System.out.println("comment jira issue ID"); 
+		System.out.println("Jenkins build and triggering CICDTEST01-3"); 
+/* base branch : DEV
+   this branch : UT1-4-jira-jenkins-sonar
+   add next line */
+		System.out.println("이슈 ID를 포함하여 Comment를 남깁니다."); 	
+	}
+
+}
